6de4586b1d8745abee0903ca9138633d4d14db45
Abdulahmid <abdulahmidevans@outlook.com>
2026-04-04 16:26:15 +0100

    Added templates and navigation system

--- app.py
+++ app.py
@@ -1,41 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def home():
-    return "Flask App is now running ;)"
+    return render_template("home.html")
 
 
 @app.route("/about")
 def about():
-    return """
-    <!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>My Flask App</title>
-        <style>
-            body {
-                font-family: Arial, sans-serif;
-                text-align: center;
-                padding: 50px;
-                background-color: #f4f4f4;
-            }
-            h1 {
-                color: #333;
-            }
-        </style>
-    </head>
-    <body>
-        <h1>Welcome to My Flask App!</h1>
-        <p>This is a very basic home page.</p>
-        <p>Server is running successfully </p>
-    </body>
-    </html>
-    """
+    return render_template("about.html")
 
 
 if __name__ == "__main__":
